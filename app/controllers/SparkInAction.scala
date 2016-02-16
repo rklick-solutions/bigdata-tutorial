b84cea1723b5c5e9d450545cf0e34ea950f894ab
Anand Singh <imanandksingh@gmail.com>
2016-02-16 18:53:56 +0530

    Added action functionality

--- app/controllers/SparkInAction.scala
+++ app/controllers/SparkInAction.scala
@@ -1,14 +1,22 @@
 package controllers
 
+import org.apache.spark.sql.DataFrame
 import play.api.mvc.{Action, Controller}
+import utils.SparkCommons
 
 /**
   * Created by anand on 2/16/16.
   */
 class SparkInAction extends Controller {
 
+  val dataFile = "resources/data.json"
+  lazy val df: DataFrame = SparkCommons.sqlContext.read.json(dataFile)
+  lazy val columns = df.columns
+
   def inAction = Action {
-    Ok(views.html.spark_in_action("Your new application is ready."))
+    val rows = df.collect()
+    val dataMap = rows.map { row => columns.map { col => (col -> row.getAs[Any](col)) }.toMap }
+    Ok(views.html.spark_in_action("home", columns, dataMap))
   }
 
 }
