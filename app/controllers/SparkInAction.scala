83d6ace0667c9ae0a1b807801c731f4a18eb0254
Anand Singh <imanandksingh@gmail.com>
2016-02-17 15:52:43 +0530

    Updated DataFrame functionality

--- app/controllers/SparkInAction.scala
+++ app/controllers/SparkInAction.scala
@@ -9,14 +9,28 @@ import utils.SparkCommons
   */
 class SparkInAction extends Controller {
 
-  val dataFile = "resources/data.json"
-  lazy val df: DataFrame = SparkCommons.sqlContext.read.json(dataFile).drop("loc")
+  val dataFile = "resources/flights.json"
+  lazy val df: DataFrame = SparkCommons.sqlContext.read.json(dataFile).withColumnRenamed("offer_valid_from", "FROM")
+    .withColumnRenamed("offer_valid_upto", "UPTO")
+    .select("COMPANY", "BANK_CODE", "CARD_TYPE", "DEAL_TYPE", "DIS_AMT", "MIN_PUR_AMT", "FROM", "UPTO")
   lazy val columns = df.columns
 
   def inAction = Action {
-    val rows = df.collect().take(10)
+    val rows = df.take(10)
     val dataMap = rows.map { row => columns.map { col => (col -> row.getAs[Any](col)) }.toMap }
-    Ok(views.html.tutorials.bigdata.spark_in_action("Spark In Action", columns, dataMap))
+    Ok(views.html.tutorials.bigdata.spark_in_action("Spark In Action", df.count, df.count, rows.size, columns, dataMap))
+  }
+
+  def applyFilter = Action {
+    val rows = df.filter(df("FROM").notEqual("0")).take(10)
+    val dataMap = rows.map { row => columns.map { col => (col -> row.getAs[Any](col)) }.toMap }
+    Ok(views.html.tutorials.bigdata.spark_in_action("Spark In Action", df.count, df.count, rows.size, columns, dataMap))
+  }
+
+  def applyDrop = Action {
+    val rows = df.na.drop().take(10)
+    val dataMap = rows.map { row => columns.map { col => (col -> row.getAs[Any](col)) }.toMap }
+    Ok(views.html.tutorials.bigdata.spark_in_action("Spark In Action", df.count, df.count, rows.size, columns, dataMap))
   }
 
 }
