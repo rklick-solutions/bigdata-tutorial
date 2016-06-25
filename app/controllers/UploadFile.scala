63e3ebf0fc93f79c47a810140c1d1fe950be717a
vedpraksh-singh <vedprak22@gmil.com>
2016-06-25 15:00:54 +0530

    Added upload page

---
+++ app/controllers/UploadFile.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import com.google.inject.Inject
+import play.api.mvc.BodyParsers.parse
+import play.api.mvc.{Action, Controller}
+import service.UploadService
+
+/**
+  * Created by ved on 25/6/16.
+  */
+class UploadFile@Inject()(uploadService: UploadService) extends Controller {
+  def file = Action {
+    Ok(views.html.tutorials.upload.upload("file upload"))
+  }
+
+
+  /**
+    *
+    * @return
+    */
+  def uploadForm = Action { implicit request =>
+    Ok(views.html.tutorials.bigdata.upload("Big Data Tutorial"))
+  }
+
+  /**
+    *
+    * @return
+    */
+
+
+  def upload = Action(parse.multipartFormData) { implicit request =>
+    val result = uploadService.uploadFile(request)
+    Ok(result)
+  }
+}
